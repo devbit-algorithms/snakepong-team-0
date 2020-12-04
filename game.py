54cb39d10b0ad7b8870e5608d62f220880277f62
Simon De Neve <simon.deneve@gmail.com>
2020-12-04 11:00:36 +0100

    playing and figuring out how to work with multidimensional arrays

--- game.py
+++ game.py
@@ -1,11 +1,34 @@
 import numpy as np
 from dllist import DoubleLinkedList
 
-# a = np.array([[i for i in range(n)] for j in range(m)])
+height = 10
+width = 10
+a = np.array([[i for i in range(height)] for j in range(width)])
+for i in range(height):
+    for j in range(width):
+        if(j == 0):       
+            a[i][j] = 0
+        if(i == 0):
+            a[i][j] = 3
 
-playingField = np.array([[i for i in range(10)] for j in range(10)])
+
+playingField = a
 
 print(playingField)
+
+# arr = [[0] * width for i in range(height)] # loop will run for the length of the outer list
+# for i in range(height):
+# # # loop will run for the length of the inner lists
+# #     for j in range(width):
+# #         if i < j:
+# #             arr[i][j] = 8
+# #         elif i > j:
+# #             arr[i][j] = 4
+# #         else:
+# #             arr[i][j] = 7
+#     for height in arr:
+#         print( ' '.join([str(x) for x in height] ) )
+
 
 pongbar = DoubleLinkedList()
 
