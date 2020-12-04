41fd498bedd4ccd12b5cba140ae92893f5d2f464
Simon De Neve <simon.deneve@gmail.com>
2020-12-04 09:51:21 +0100

    added ReadME and trying to visualise snakepong

---
+++ game.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+matrix = np.arange(100).reshape(10, 10)
+
+print(matrix)
+
+# class Pong:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+
+#     def getx(self):
+#         return self.__x
+
+#     def gety(self):
+#         return self.__x
+
+# class Snake:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+    
+#     def getx(self):
+#         return self.__x
+
+#     def gety(self):
+#         return self.__x
+
+# class Ball:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+
+#     def getx(self):
+#         return self.__x
+
+#     def gety(self):
+#         return self.__x
+
+# class Field:
+#     def __init__(self, width, height):
+#         self.__x = width
+#         self.__y = height
+    
+#     def getx(self):
+#         return self.__x
+
+#     def gety(self):
+#         return self.__x
