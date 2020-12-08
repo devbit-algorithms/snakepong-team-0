41e59729263bc75b3dfb50b55b5ade68f0c5a7e1
Simon De Neve <simon.deneve@gmail.com>
2020-12-08 11:48:00 +0100

    commiting the field

--- field.py
+++ field.py
@@ -1,4 +1,18 @@
+import numpy as np
+
 class Field:
     def __init__(self, width = 30, height = 30):
         self.width = width
         self.height = height
+        self.screen = np.array([[i for i in range(height)] for j in range(width)])
+
+    def clear(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                self.screen[i][j] = 0
+    
+
+
+field = Field()
+field.clear()
+print(field.screen)
