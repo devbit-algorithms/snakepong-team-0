d5754b4e6dc82de9f40fe2834d5c3dbfa717a85b
Simon De Neve <simon.deneve@gmail.com>
2020-12-08 12:04:57 +0100

    commiting output_to_terminal

--- field.py
+++ field.py
@@ -4,15 +4,25 @@ class Field:
     def __init__(self, width = 30, height = 30):
         self.width = width
         self.height = height
-        self.screen = np.array([[i for i in range(height)] for j in range(width)])
+        self.screen = np.array([[i for i in range(height)] for j in range(width)], dtype=str)
 
     def clear(self):
         for i in range(self.height):
             for j in range(self.width):
-                self.screen[i][j] = 0
+                self.screen[i][j] = ' '
     
+    def draw_symbol(self, x, y, symbol):
+        self.screen[x][y] = symbol
+    
+    def output_to_terminal(self):
+        for j in range(self.width):
+            for i in range(self.height):
+                print(self.screen[i][j], end=' ')
+            print('\n')
+        print('\n')       
+                
 
 
 field = Field()
 field.clear()
-print(field.screen)
+field.output_to_terminal()
