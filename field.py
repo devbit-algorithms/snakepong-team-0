e75295f9e8378c59ac09af8dc0ec7bdaa9b609b5
TrikThom <thomas@trikthom.com>
2020-12-08 11:33:41 +0100

    Added wall class and changes field class

--- field.py
+++ field.py
@@ -1,5 +1,4 @@
-from entity import Entity
-
-class Field(Entity):
-    def __init__(self, x, y):
-        super().__init__(x, y)
+class Field:
+    def __init__(self, width = 30, height = 30):
+        self.width = width
+        self.height = height
---
+++ wall.py
@@ -0,0 +1,5 @@
+from entity import Entity
+
+class Wall(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
