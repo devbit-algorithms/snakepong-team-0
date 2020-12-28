9b0b7ac0cf0ce6ce17d9f2ce6b9eabe6888b8bd5
Thomas Crombez <thomas@trikthom.com>
2020-12-28 12:04:01 +0100

    Fixed tests

--- pong.py
+++ pong.py
@@ -5,5 +5,11 @@ class Pong(Entity):
     def __init__(self, x, y):
         super().__init__(x, y)
 
+    def update_pong(self, ball):
+        if ball.get_y() > self.get_y():
+            self.move_down()
+        if ball.get_y() < self.get_y():
+            self.move_up()
+
     def render(self, field):
         field.draw_symbol(self.get_x(), self.get_y(), EntitySymbol(self, "\u0023", 1))
